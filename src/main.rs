89baf3ebe6e702fc8c0f16be402c980dccbc2335
lsci <lsci@lle.rochester.edu>
2023-07-26 13:59:56 -0400

    alot

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,7 @@ struct Simulator {
     PCSampler: Option<PCSampler>,
     step: u64,
     force_evictions: u64,
+    total_tenancy_remaining: u64,
 }
 
 impl Simulator {
@@ -80,6 +81,7 @@ impl Simulator {
             PCSampler: None,
             step: 0,
             force_evictions: 0,
+            total_tenancy_remaining: 0,
         }
     }
 
@@ -107,6 +109,9 @@ impl Simulator {
             self.force_evictions += 1;
             self.PCS -= 1;
         }
+        // } else {
+        //     self.FERemainingTracker.insert(0, self.FERemainingTracker.get(&0).unwrap_or(&0) + 1);
+        // }
     }
 
     fn force_evcit(&mut self) {
@@ -114,12 +119,7 @@ impl Simulator {
         let keyToDecrement = self.PCSampler.as_mut().unwrap().sample();
         self.PCExpiringBlockTracker.insert(keyToDecrement, self.PCExpiringBlockTracker.get(&keyToDecrement).unwrap() - 1);
         self.FERemainingTracker.insert(keyToDecrement - self.step, self.FERemainingTracker.get(&(keyToDecrement- self.step)).unwrap_or(&0) + 1);
-    }
-
-    fn normalize_map(&mut self) -> Vec<(u64, f64)> {
-        // println!("FE remaining {:#?}", self.FERemainingTracker);
-        let total_FE_remaining: u64 = self.FERemainingTracker.iter().map(|(_, value)| *value).sum();
-        self.FERemainingTracker.iter().map(|(key, value)| (*key, *value as f64 / total_FE_remaining as f64)).collect()
+        self.total_tenancy_remaining += (keyToDecrement - self.step);
     }
 
     fn get_excess(&self, fixed: u64) -> u64 {
@@ -139,51 +139,74 @@ fn expectation(mut vec: Vec<(u64, f64)>) -> f64 {
     vec.iter().map(|(key, value)| *key as f64 * *value).sum::<f64>()
 }
 
-fn caching(ten_dist: Sampler, cache_size: u64, delta: f64) -> (u64, u64, u64, Vec<(u64, f64)>) {
+fn normalize_map(map: HashMap<u64, u64>) -> Vec<(u64, f64)> {
+    // println!("FE remaining {:#?}", self.FERemainingTracker);
+    let total: u64 = map.iter().map(|(_, value)| *value).sum();
+    map.iter().map(|(key, value)| (*key, *value as f64 / total as f64)).collect()
+}
+
+fn caching(ten_dist: Sampler, cache_size: u64, delta: f64) -> (u64, u64, u64, Vec<(u64, f64)>, u64) {
     let mut cache = Simulator::init();
     let mut trace_len: u64 = 0;
-    let mut samples_to_issue: u64 = 1024*64;
+    let mut samples_to_issue: u64 = 1024*1024*8;
     let mut prev_output: Option<f64> = None;
     let mut total_overalloc: u64 = 0;
-    loop {
-        for _ in 0..samples_to_issue {
-            trace_len += 1;
-            let tenancy = ten_dist.sample();
-            cache.add_tenancy(tenancy, cache_size);
-            total_overalloc += cache.get_excess(cache_size);
-        }
-        // if prev_output.is_some()
-        //     && ((total_overalloc as f64) / (trace_len as f64) - prev_output.unwrap()) < delta
-        // {
-        if prev_output.is_some()
-            && expectation(cache.normalize_map()) - prev_output.unwrap() < delta
-        {
-            return (total_overalloc, trace_len, cache.force_evictions, cache.normalize_map());
-        }
-        // prev_output = Some((total_overalloc as f64) / (trace_len as f64));
-        prev_output = Some(expectation(cache.normalize_map()));
-        samples_to_issue *= 2;
+    for _ in 0..samples_to_issue {
+        trace_len += 1;
+        let tenancy = ten_dist.sample();
+        cache.add_tenancy(tenancy, cache_size);
+        total_overalloc += cache.get_excess(cache_size);
     }
+    return (total_overalloc, trace_len, cache.force_evictions, normalize_map(cache.FERemainingTracker), cache.total_tenancy_remaining);
+    // prev_output = Some((total_overalloc as f64) / (trace_len as f64));
+}
+
+fn generate_imperical_tenancy_remaining_dist(data: &Vec<(u64, f64)>, PCS: u64, FER: f64) -> Vec<(u64, f64)> {
+    let mut map: HashMap<u64, f64> = HashMap::new();
+    let maxTenancy = data.into_iter().fold(0, |acc, (tenancy, _)| acc.max(*tenancy));
+    (0..maxTenancy).for_each(|i| {
+        let mut total = 0.0;
+        data.into_iter().for_each(|(tenancy, value)| {
+            if *tenancy > i {
+                // if i == 0 {
+                //     println!("tenancy {:#?} i {:#?}", tenancy, i);
+                // }
+                total += f64::powf(((PCS-1) as f64 / PCS as f64), FER*(*tenancy as f64-i as f64-1.0)) * value*(1.0/PCS as f64)*FER;
+            }
+        });
+        if i == 0 {
+            map.insert(i, 1.0-FER);
+        } else {
+            map.insert(i, total);
+        }
+
+    });
+    map.into_iter().collect()
+
 }
 
 fn main() {
     let data = vec![
-        (1, 0.3333),
-        (5, 0.3333),
-        (150, 0.333),
+        (1000, 0.5),
+        (2, 0.5)
     ];
     // let x = vec![1, 2, 3];
     // let y = vec![1.0, 2.0, 3.0];
     // let iterator = x.into_iter().zip(y.into_iter());
-    let (over_alloc, trace_len, forced_evictions, FE_remaining_dist) = caching(Sampler::new(data.into_iter()), 10, 0.00000001);
-
+    let (over_alloc, trace_len, forced_evictions, FE_remaining_dist, total_tenancy_remianing) = caching(Sampler::new(data.clone().into_iter()), 501, 0.00000001);
+    // \nImperical FE_remaining_dist: {:#?}
     println!(
-        "over_alloc: {},\ntrace_len: {},\ndiv : {},\nforced_evictions : {:#?},\nE[FE_remaining_dist]: {}",
+        "over_alloc: {},\ntrace_len: {},\ndiv : {},\nforced_evictions : {:#?},\nFE_remaining_dist: {:#?}, \nE[FE_remaining_dist]: {}, \nFE_ratio: {}, \ntenancy remaining per eviction: {}, \ntenancy remaining per access: {}",
         over_alloc,
         trace_len,
         over_alloc as f64 / trace_len as f64,
         forced_evictions,
         // FE_remaining_dist ,
-        FE_remaining_dist.iter().map(|(key, value)| *key as f64 * *value).sum::<f64>()
+        "fe dist",
+        FE_remaining_dist.iter().map(|(key, value)| *key as f64 * *value).sum::<f64>(),
+        forced_evictions as f64 / trace_len as f64,
+        total_tenancy_remianing as f64 / forced_evictions as f64,
+        total_tenancy_remianing as f64 / trace_len as f64,
+        // generate_imperical_tenancy_remaining_dist(&data, 5, forced_evictions as f64 / trace_len as f64),
     );
 }
